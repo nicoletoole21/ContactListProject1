323a2c5f1881b253676a35565faf8b4cc300af1f
Nicole Toole <nicoletoole@nicoles-mbp.wifi.gsu.edu>
2022-01-26 13:08:10 -0500

    Toggle Enabled

--- app/src/main/java/com/example/contactlistproject/MainActivity.java
+++ app/src/main/java/com/example/contactlistproject/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.contactlistproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +17,95 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initListButton();
+        initMapButton();
+        initSettingsButton();
+
+        initToggleButton();
+        setForEditing(false);
 
     }
+
+    private void initListButton() {
+        ImageButton ibList = findViewById(R.id.imageButtonList);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //reference created for current activity and which activity to start)
+                Intent intent = new Intent (MainActivity.this, ContactListActivity.class);
+                //intent flag set to alert the operating system to not make multiple copies of same activity
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initMapButton() {
+        ImageButton ibList = findViewById(R.id.imageButtonSettings);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //reference created for current activity and which activity to start)
+                Intent intent = new Intent (MainActivity.this, ContactMapActivity.class);
+                //intent flag set to alert the operating system to not make multiple copies of same activity
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initSettingsButton() {
+        ImageButton ibList = findViewById(R.id.imageButtonSettings);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //reference created for current activity and which activity to start)
+                Intent intent = new Intent (MainActivity.this, ContactSettingsActivity.class);
+                //intent flag set to alert the operating system to not make multiple copies of same activity
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initToggleButton() {
+        final ToggleButton editToggle = (ToggleButton) findViewById(R.id.toggleButtonEdit);
+        editToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                setForEditing(editToggle.isChecked());
+
+
+            }
+        });
+    }
+
+    private void setForEditing(boolean enabled) {
+        EditText editName = findViewById(R.id.editName);
+        EditText editAddress = findViewById(R.id.editAddress);
+        EditText editCity = findViewById(R.id.editCity);
+        EditText editState = findViewById(R.id.editState);
+        EditText editZipcode = findViewById(R.id.editZipcode);
+        EditText editPhone = findViewById(R.id.editHome);
+        EditText editCell = findViewById(R.id.editCell);
+        EditText editEmail = findViewById(R.id.editEMail);
+        Button buttonChange = findViewById(R.id.buttonBirthday);
+        Button buttonSave = findViewById(R.id.butttonSave);
+
+        editName.setEnabled(enabled);
+        editAddress.setEnabled(enabled);
+        editCity.setEnabled(enabled);
+        editState.setEnabled(enabled);
+        editZipcode.setEnabled(enabled);
+        editPhone.setEnabled(enabled);
+        editCell.setEnabled(enabled);
+        editEmail.setEnabled(enabled);
+        buttonChange.setEnabled(enabled);
+        buttonSave.setEnabled(enabled);
+
+        if (enabled) {
+            editName.requestFocus();
+        }
+
+
+    }
+
 }
