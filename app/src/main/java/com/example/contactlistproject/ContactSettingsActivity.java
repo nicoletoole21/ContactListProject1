f2ffe533e612b74a7bae547927b1ddd476c86af3
chaseglover <cglover17>
2022-02-02 11:10:24 -0500

    Change Birthday

--- app/src/main/java/com/example/contactlistproject/ContactSettingsActivity.java
+++ app/src/main/java/com/example/contactlistproject/ContactSettingsActivity.java
@@ -2,7 +2,20 @@ package com.example.contactlistproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class ContactSettingsActivity extends AppCompatActivity {
 
@@ -10,5 +23,45 @@ public class ContactSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_settings);
+        initSettings();
     }
-}
+    private void initListButton() {
+        ImageButton ibList = findViewById(R.id.imageButtonList);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //reference created for current activity and which activity to start)
+                Intent intent = new Intent (ContactSettingsActivity.this, ContactListActivity.class);
+                //intent flag set to alert the operating system to not make multiple copies of same activity
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initMapButton() {
+        ImageButton ibList = findViewById(R.id.imageButtonSettings);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //reference created for current activity and which activity to start)
+                Intent intent = new Intent (ContactSettingsActivity.this, ContactMapActivity.class);
+                //intent flag set to alert the operating system to not make multiple copies of same activity
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initSettingsButton() {
+
+        ImageButton ibSettings = findViewById(R.id.imageButtonSettings);
+
+        ibSettings.setEnabled(false);
+
+    }
+    private void initSettings{
+        String sortBy = getSharedPreferences("MyContactListPreferences",Context.MODE_PRIVATE).getString("sortfield",:"contactname");
+    String sortOrder = getSharedPreferences("MyContactListPreferences",Context.MODE_PRIVATE).getString("sortorder","ASC");
+
+    RadioButton rbName = findViewById(R.id.)
+    }
+            }
