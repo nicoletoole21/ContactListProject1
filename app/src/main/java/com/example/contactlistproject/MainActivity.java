a6174cf87a4f8b59bd2b73315d2b26f608632d32
chaseglover <cglover17>
2022-02-02 13:44:51 -0500

    main

--- app/src/main/java/com/example/contactlistproject/MainActivity.java
+++ app/src/main/java/com/example/contactlistproject/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +17,7 @@ import android.widget.ToggleButton;
 import java.util.Calendar;
 public class MainActivity extends AppCompatActivity implements DatePickerDialog.SaveDateListener {
     private Contact currentContact;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +31,7 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         initToggleButton();
         setForEditing(false);
         initChangeDateButton();
-initTextChangedEvents;
+        initTextChangedEvents();
 
     }
 
@@ -37,7 +40,7 @@ initTextChangedEvents;
         ibList.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 //reference created for current activity and which activity to start)
-                Intent intent = new Intent (MainActivity.this, ContactListActivity.class);
+                Intent intent = new Intent(MainActivity.this, ContactListActivity.class);
                 //intent flag set to alert the operating system to not make multiple copies of same activity
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
@@ -50,7 +53,7 @@ initTextChangedEvents;
         ibList.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 //reference created for current activity and which activity to start)
-                Intent intent = new Intent (MainActivity.this, ContactMapActivity.class);
+                Intent intent = new Intent(MainActivity.this, ContactMapActivity.class);
                 //intent flag set to alert the operating system to not make multiple copies of same activity
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
@@ -63,7 +66,7 @@ initTextChangedEvents;
         ibList.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 //reference created for current activity and which activity to start)
-                Intent intent = new Intent (MainActivity.this, ContactSettingsActivity.class);
+                Intent intent = new Intent(MainActivity.this, ContactSettingsActivity.class);
                 //intent flag set to alert the operating system to not make multiple copies of same activity
 
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -120,9 +123,8 @@ initTextChangedEvents;
     public void didFinishDatePickerDialog(Calendar selectedTime) {
         TextView birthday =
                 findViewById(R.id.textBirthday);
-         birthday.setText(DateFormat.format("MM/dd/yyyy", selectedTime));
+        birthday.setText(DateFormat.format("MM/dd/yyyy", selectedTime));
         currentContact.setBirthday(selectedTime);
-
 
 
     }
@@ -134,10 +136,43 @@ initTextChangedEvents;
             public void onClick(View view) {
                 FragmentManager fm = getSupportFragmentManager();
                 DatePickerDialog datePickerDialog = new DatePickerDialog();
-                datePickerDialog.show(fm,"DatePick");
+                datePickerDialog.show(fm, "DatePick");
             }
-            private
+
         });
+
     }
+
+    private void initTextChangedEvents() {
+        final EditText etContactName = findViewById(R.id.editName);
+        etContactName.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+                currentContact.setContactName(etContactName.getText().toString());
+            }
+
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
+        final EditText etStreetAddress = findViewById(R.id.editAddress);
+        etStreetAddress.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                currentContact.setStreetAddress(etStreetAddress.getText().toString());
+            }
+
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
+
+    }
+
 }
 
